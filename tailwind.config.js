ce9ad973182bda9d2620ec418fe33ac769aba11c
jonathastavares <jonathastavares@id.uff.br>
2022-10-30 21:09:17 -0300

    Configurações customizadas do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fundo: '#B6A1C4',
+        letra: '#3E3147',
+        primaria: '#665175',
+        secundaria: '#6B398F',
+        terciaria: '#A986C2',
+      },
+    },
   },
   plugins: [],
 };
