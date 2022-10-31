dd3bc310ebb4a39a16848d856cb71a1ebb58fa75
anamoraisdev <anamorais.dev@gmail.com>
2022-10-30 22:10:47 -0300

    Criação da navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   // <Link to="/">Texto do link</Link>
-  return <div></div>;
+  return (
+    <nav className='flex gap-4 justify-between px-6 py-4 items-center font-bold'>
+      <Link to="/" className='text-letra hover:text-secundaria'> 
+          <img src="./estadisticas.png" alt="logo"className=' w-[50px]'/> 
+      </Link>
+      <Link to="/login" className='px-6 py-2 bg-secundaria hover:bg-primaria text-white rounded-md'> Login </Link>
+    </nav>
+  )
 };
 
 export default Navbar;
